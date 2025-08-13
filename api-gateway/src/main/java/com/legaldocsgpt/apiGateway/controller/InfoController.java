d2ccf97d128cb9646e3ba1a221d7f6d969e78aa5
Alex <bojkosasa78@gmail.com>
2025-08-13 20:30:14 +0200

    Misc

--- api-gateway/src/main/java/com/legaldocsgpt/apiGateway/controller/InfoController.java
+++ api-gateway/src/main/java/com/legaldocsgpt/apiGateway/controller/InfoController.java
@@ -1,6 +1,5 @@
 package com.legaldocsgpt.apiGateway.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +7,7 @@ import java.util.Map;
 
 @RestController
 public class InfoController {
+
     @GetMapping("/api/info")
     public Map<String, String> getInfo() {
         return Map.of(

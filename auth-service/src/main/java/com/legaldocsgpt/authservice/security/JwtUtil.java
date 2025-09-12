ee13e87cd7cf449ccb9ff77145ef2daebaa399f2
Alex <bojkosasa78@gmail.com>
2025-09-12 16:19:17 +0200

    Token generation modernization for security

--- auth-service/src/main/java/com/legaldocsgpt/authservice/security/JwtUtil.java
+++ auth-service/src/main/java/com/legaldocsgpt/authservice/security/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.legaldocsgpt.authservice.security;
 
+import com.legaldocsgpt.authservice.dto.UserInfoResponseDto;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -21,22 +23,30 @@ public class JwtUtil {
         this.expiration = expiration;
     }
 
-    public String generateToken(String username) {
+    public String generateToken(String username, String email, String role ) {
         return Jwts.builder()
                 .subject(username)
+                .claims(Map.of(
+                        "email", email,
+                        "role", role
+                ))
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key, Jwts.SIG.HS256)
                 .compact();
     }
 
-    public String validateToken(String token) {
+    public UserInfoResponseDto validateToken(String token) {
         try {
-            return Jwts.parser()
+            Claims claims = Jwts.parser()
                     .verifyWith(key)
                     .build()
                     .parseSignedClaims(token)
-                    .getPayload()
-                    .getSubject();
+                    .getPayload();
+            String username = claims.getSubject();
+            String email = claims.get("email", String.class);
+            String role = claims.get("role", String.class);
+
+            return new UserInfoResponseDto(email, username, role);
 
         } catch (JwtException e) {
             return null;

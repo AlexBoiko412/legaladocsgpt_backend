905d119f3f671dc790681393639f91014dcbb73a
Alex <bojkosasa78@gmail.com>
2025-12-16 14:02:43 +0100

    init mock

--- document-generator/src/main/java/com/legaldocsgpt/documentgenerator/service/DocumentService.java
+++ document-generator/src/main/java/com/legaldocsgpt/documentgenerator/service/DocumentService.java
@@ -6,7 +6,6 @@ import com.legaldocsgpt.documentgenerator.dto.TemplateInfo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class DocumentService {

7234673a18af54d88c3c860a7f8fbc9dc476fd4f
Alex <bojkosasa78@gmail.com>
2025-12-22 11:17:48 +0100

    RabbitMQ implementing adding document worker

---
+++ document-worker/src/main/java/com/legaldocsgpt/documentworker/DocumentWorkerApplication.java
@@ -0,0 +1,17 @@
+package com.legaldocsgpt.documentworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.legaldocsgpt")
+@EnableJpaRepositories(basePackages = "com.legaldocsgpt")
+@EntityScan(basePackages = "com.legaldocsgpt")
+public class DocumentWorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentWorkerApplication.class, args);
+    }
+}
---
+++ document-worker/src/test/java/com/legaldocsgpt/documentworker/DocumentWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legaldocsgpt.documentworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

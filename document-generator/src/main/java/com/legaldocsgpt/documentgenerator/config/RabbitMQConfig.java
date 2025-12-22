26cd0cdd3f267b9eae66358031b2eb775044f0ee
Alex <bojkosasa78@gmail.com>
2025-12-22 11:20:34 +0100

    document-generator rabbitmq config

---
+++ document-generator/src/main/java/com/legaldocsgpt/documentgenerator/config/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package com.legaldocsgpt.documentgenerator.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "document_generation_queue";
+    public static final String EXCHANGE_NAME = "document_exchange";
+    public static final String ROUTING_KEY = "document_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}

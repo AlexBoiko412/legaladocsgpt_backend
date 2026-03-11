7063b3e6fb7fa1189865c13c48a3bb645b71862a
Alex <bojkosasa78@gmail.com>
2026-03-11 11:28:54 +0100

    storage application

--- storage-service/src/main/java/com/legaldocsgpt/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/com/legaldocsgpt/storageservice/StorageServiceApplication.java
@@ -2,26 +2,16 @@ package com.legaldocsgpt.storageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.legaldocsgpt")
 public class StorageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageServiceApplication.class, args);
-    }
-
-    @Configuration
-    static class WebConfig implements WebFluxConfigurer {
-        @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            // Map /api/storage/docs/** to the physical folder on the server
-            registry.addResourceHandler("/docs/**")
-                    .addResourceLocations("file:/app/storage/docs/");
-        }
     }
 
     @RestController

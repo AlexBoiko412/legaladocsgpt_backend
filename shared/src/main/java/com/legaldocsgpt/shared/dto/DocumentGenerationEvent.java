3d4014d316b2aaf9248fbdc984700dedd7dbad23
Alex <bojkosasa78@gmail.com>
2026-03-11 11:30:09 +0100

    added field doc path

--- shared/src/main/java/com/legaldocsgpt/shared/dto/DocumentGenerationEvent.java
+++ shared/src/main/java/com/legaldocsgpt/shared/dto/DocumentGenerationEvent.java
@@ -12,6 +12,7 @@ public class DocumentGenerationEvent {
     private String jobId;
     private String userId;
     private String templateId;
+    private String docxPath;
     private String format;
     private Map<String, String> data;
 }

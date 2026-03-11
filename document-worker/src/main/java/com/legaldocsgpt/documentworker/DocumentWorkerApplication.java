ea65622b16963169ec313727b6a28a92c0630f9a
Alex <bojkosasa78@gmail.com>
2026-03-11 11:36:50 +0100

    misc

--- document-worker/src/main/java/com/legaldocsgpt/documentworker/DocumentWorkerApplication.java
+++ document-worker/src/main/java/com/legaldocsgpt/documentworker/DocumentWorkerApplication.java
@@ -4,17 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.legaldocsgpt")
-@EnableJpaRepositories(basePackages = {
-        "com.legaldocsgpt.shared.repository"
+@SpringBootApplication
+@ComponentScan(basePackages = "com.legaldocsgpt")
+@EnableFeignClients(basePackages = {
+        "com.legaldocsgpt.documentworker.client",
+        "com.legaldocsgpt.shared.client"
 })
-@EntityScan(basePackages = {
-        "com.legaldocsgpt.documentworker.entity",
-        "com.legaldocsgpt.shared.entity"
-})
-@EnableFeignClients(basePackages = "com.legaldocsgpt.shared.client")
+@EnableJpaRepositories(basePackages = "com.legaldocsgpt.shared.repository")
+@EntityScan(basePackages = "com.legaldocsgpt.shared.entity")
 public class DocumentWorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentWorkerApplication.class, args);
